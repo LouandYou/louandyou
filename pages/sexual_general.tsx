99fdb03ae85d5b7216c710a624b97d2a5a115abd
Nadav Kirsh <saturdaysyouth@gmail.com>
2021-12-08 17:26:30 +0100

    renaming files

--- pages/sexual_general.tsx
+++ pages/sexual_general.tsx
@@ -1,14 +1,15 @@
 import { NextPage } from "next";
 import Image from "next/image";
 import and_logo from "../public/and_logo.png";
+import styles from "../styles/Home.module.scss";
 
-const sexual_violence: NextPage = () => {
+const sexual_general: NextPage = () => {
     return (
         <>
             <Image src={and_logo} width="110" height="110" alt="logo" />
-            <h1>Sexual Violance</h1>
+            <h1 className={styles.title}> Sexual Violance</h1>
         </>
     );
 };
 
-export default sexual_violence;
+export default sexual_general;
