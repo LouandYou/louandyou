c8fa6eb18a3e5d1d415fe1d66671b47d49656316
Nadav Kirsh <saturdaysyouth@gmail.com>
2021-12-08 17:25:46 +0100

    checkbox component update

--- components/Checkbox.tsx
+++ components/Checkbox.tsx
@@ -3,22 +3,26 @@ import React from "react";
 interface Props {
     className?: string;
     label: string;
-    value: boolean;
+    value?: boolean;
     onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-function Checkbox({ className, label, value, onChange }: Props) {
-    return (
-        <label className={className}>
-            <input
-                className="mr-1"
-                type="checkbox"
-                checked={value}
-                onChange={onChange}
-            />
-            {label}
-        </label>
-    );
-}
+const Checkbox = React.forwardRef(
+    ({ className, label, value, onChange }: Props, ref: any) => {
+        return (
+            <label className={className}>
+                <input
+                    className="mr-1"
+                    type="checkbox"
+                    ref={ref}
+                    checked={value}
+                    onChange={onChange}
+                />
+                {label}
+            </label>
+        );
+    }
+);
 
+Checkbox.displayName = "Checkbox";
 export default Checkbox;
