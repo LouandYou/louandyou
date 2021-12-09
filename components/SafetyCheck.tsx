2cd22b11aeba8b6c518401dccf1aa85db7b2205f
Nadav Kirsh <saturdaysyouth@gmail.com>
2021-12-09 16:40:16 +0100

    remove "yes" option

--- components/SafetyCheck.tsx
+++ components/SafetyCheck.tsx
@@ -14,56 +14,54 @@ const SafetyCheck = () => {
     const onClickOne = () => {
         checkbox3.current!.checked = false;
         checkbox2.current!.checked = false;
-        setSafety(safety === "yes" ? "" : "yes");
     };
 
     const onClickTwo = () => {
         checkbox3.current!.checked = false;
         checkbox1.current!.checked = false;
         setSafety(safety === "no" ? "" : "no");
+        if (checkbox2.current!.checked) {
+            toggleIsOpen();
+        }
     };
     const onClickThree = () => {
         checkbox1.current!.checked = false;
         checkbox2.current!.checked = false;
         setSafety(safety === "unsure" ? "" : "unsure");
+        if (checkbox3.current!.checked) {
+            toggleIsOpen();
+        }
     };
 
-    const handleClick = () => isOpen ? setIsOpen(false) : setIsOpen(true);
+    const toggleIsOpen = () => {
+        isOpen ? setIsOpen(false) : setIsOpen(true);
+    };
 
     return (
         <>
-            <div className={styles.get_help}>
-                <div className="card-content">
-                    <p className="py-2">Are you safe right now?</p>
-                    <Checkbox
-                        className="checkbox"
-                        ref={checkbox1}
-                        label="Yes"
-                        onChange={onClickOne}
-                    />
-                    <Checkbox
-                        className="checkbox px-2"
-                        label="No"
-                        ref={checkbox2}
-                        onChange={onClickTwo}
-                    />
-                    <Checkbox
-                        className="checkbox px-2 "
-                        label="Not sure"
-                        ref={checkbox3}
-                        onChange={onClickThree}
-                    />
-                </div>
-                <div className="card-content">
-                    <button
-                        className="button is-fullwidth"
-                        onClick={handleClick}
-                    >
-                        Submit
-                    </button>
-                </div>
+            <div className="card-content">
+                <p className="py-2">Are you safe right now?</p>
+                <Checkbox
+                    className="checkbox"
+                    ref={checkbox1}
+                    label="Yes"
+                    onChange={onClickOne}
+                />
+                <Checkbox
+                    className="checkbox px-2"
+                    label="No"
+                    ref={checkbox2}
+                    onChange={onClickTwo}
+                />
+                <Checkbox
+                    className="checkbox px-2 "
+                    label="Not sure"
+                    ref={checkbox3}
+                    onChange={onClickThree}
+                />
             </div>
-            {isOpen && <SafetyPopup onClick={handleClick} safety={safety} />}
+
+            {isOpen && <SafetyPopup onClick={toggleIsOpen} safety={safety} />}
         </>
     );
 };
