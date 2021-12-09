f01d41506d17223967555aee89fc9451bf470c64
Nadav Kirsh <saturdaysyouth@gmail.com>
2021-12-09 16:39:43 +0100

    add "both" checkbox that acts like "domestic"

--- components/GetHelp.tsx
+++ components/GetHelp.tsx
@@ -1,11 +1,39 @@
-import React, {ReactElement, useState} from "react";
+import React, { ReactElement, useRef, useState } from "react";
 import Checkbox from "./Checkbox";
 import styles from "../styles/GetHelp.module.scss";
 import Link from "next/link";
+import SafetyCheck from "./SafetyCheck";
 
 function GetHelp(): ReactElement {
     const [isSexual, setIsSexual] = useState<boolean>();
     const [isLongAgo, setIsLongAgo] = useState<boolean>();
+
+    const checkbox1 = useRef<HTMLInputElement>();
+    const checkbox2 = useRef<HTMLInputElement>();
+    const checkbox3 = useRef<HTMLInputElement>();
+
+    const onClickOne = () => {
+        checkbox3.current!.checked = false;
+        checkbox2.current!.checked = false;
+        if (checkbox1.current!.checked) {
+            setIsSexual(true);
+        }
+    };
+
+    const onClickTwo = () => {
+        checkbox3.current!.checked = false;
+        checkbox1.current!.checked = false;
+        if (checkbox2.current!.checked) {
+            setIsSexual(false);
+        }
+    };
+    const onClickThree = () => {
+        checkbox1.current!.checked = false;
+        checkbox2.current!.checked = false;
+        if (checkbox3.current!.checked) {
+            setIsSexual(false);
+        }
+    };
 
     const getHref = () => {
         if (isSexual && !isLongAgo) {
@@ -22,49 +50,54 @@ function GetHelp(): ReactElement {
     const canSubmit = () => isSexual !== undefined && isLongAgo !== undefined;
 
     return (
-        <>
-            <div className={styles.get_help}>
-                <div className="card-content">
-                    <p className="py-2">
-                        What type of violence did you experience?
-                    </p>
-                    <Checkbox
-                        className="checkbox"
-                        label="Sexual"
-                        value={isSexual === true}
-                        onChange={() => setIsSexual(true)}
-                    />
-                    <Checkbox
-                        className="checkbox px-2"
-                        label="Domestic"
-                        value={isSexual === false}
-                        onChange={() => setIsSexual(false)}
-                    />
-                </div>
-                <div className="card-content">
-                    <p className="py-2 ">How long ago did it happen?</p>
-                    <Checkbox
-                        className="checkbox"
-                        label="Long ago"
-                        value={isLongAgo === true}
-                        onChange={() => setIsLongAgo(true)}
-                    />
-                    <Checkbox
-                        className="checkbox px-2"
-                        label="Last 7 days"
-                        value={isLongAgo === false}
-                        onChange={() => setIsLongAgo(false)}
-                    />
-                </div>
-                <div className="card-content">
-                    {
-                        canSubmit() && <Link href={getHref()} passHref>
-                            <button className="button is-fullwidth">Submit</button>
-                        </Link>
-                    }
-                </div>
+        <div className={styles.get_help}>
+            <SafetyCheck />
+            <div className="card-content">
+                <p className="py-2">
+                    What type of violence did you experience?
+                </p>
+                <Checkbox
+                    className="checkbox"
+                    label="Sexual"
+                    ref={checkbox1}
+                    onChange={onClickOne}
+                />
+                <Checkbox
+                    className="checkbox px-2"
+                    label="Domestic"
+                    ref={checkbox2}
+                    onChange={onClickTwo}
+                />
+                <Checkbox
+                    className="checkbox px-2"
+                    label="Both"
+                    ref={checkbox3}
+                    onChange={onClickThree}
+                />
             </div>
-        </>
+            <div className="card-content">
+                <p className="py-2 ">How long ago did it happen?</p>
+                <Checkbox
+                    className="checkbox px-2"
+                    label="Last 7 days"
+                    value={isLongAgo === false}
+                    onChange={() => setIsLongAgo(false)}
+                />
+                <Checkbox
+                    className="checkbox"
+                    label="Long ago"
+                    value={isLongAgo === true}
+                    onChange={() => setIsLongAgo(true)}
+                />
+            </div>
+            <div className="card-content">
+                {canSubmit() && (
+                    <Link href={getHref()} passHref>
+                        <button className="button is-fullwidth">Submit</button>
+                    </Link>
+                )}
+            </div>
+        </div>
     );
 }
 
