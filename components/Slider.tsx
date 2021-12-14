c63f82ce402061cd25543e847bdd1a70b7f87422
Nadav Kirsh <saturdaysyouth@gmail.com>
2021-12-14 17:02:49 +0100

    add padding

--- components/Slider.tsx
+++ components/Slider.tsx
@@ -10,17 +10,19 @@ interface Props {
 const Slider = React.forwardRef(
   ({ cont, label, onChange }: Props, ref: any) => {
     return (
-      //@ts-ignore
-      <div cont={cont} className={styles.switch_button}>
-        <input
-          onChange={onChange}
-          ref={ref}
-          className={styles.switch_button_checkbox}
-          type="checkbox"
-        ></input>
-        <label className={styles.switch_button_label} htmlFor="">
-          <span className={styles.switch_button_label_span}>{label}</span>
-        </label>
+      <div className="block py-1">
+        {/* @ts-ignore */}
+        <div cont={cont} className={styles.switch_button}>
+          <input
+            onChange={onChange}
+            ref={ref}
+            className={styles.switch_button_checkbox}
+            type="checkbox"
+          ></input>
+          <label className={styles.switch_button_label} htmlFor="">
+            <span className={styles.switch_button_label_span}>{label}</span>
+          </label>
+        </div>
       </div>
     );
   }
