7e8b5f78b12301b784736e521667fb9d305427f2
Nadav Kirsh <saturdaysyouth@gmail.com>
2021-12-16 17:35:24 +0100

    safety check update

--- components/SafetyCheck.tsx
+++ components/SafetyCheck.tsx
@@ -4,66 +4,68 @@ import SafetyPopup from "./SafetyPopup";
 import styles from "../styles/Home.module.scss";
 
 const SafetyCheck = () => {
-    const [isOpen, setIsOpen] = useState<boolean>(false);
-    const [safety, setSafety] = useState<string>("");
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [safety, setSafety] = useState<string>("");
 
-    const checkbox1 = useRef<HTMLInputElement>();
-    const checkbox2 = useRef<HTMLInputElement>();
-    const checkbox3 = useRef<HTMLInputElement>();
+  const checkbox1 = useRef<HTMLInputElement>();
+  const checkbox2 = useRef<HTMLInputElement>();
+  const checkbox3 = useRef<HTMLInputElement>();
 
-    const onClickOne = () => {
-        checkbox3.current!.checked = false;
-        checkbox2.current!.checked = false;
-    };
+  const onClickOne = () => {
+    checkbox3.current!.checked = false;
+    checkbox2.current!.checked = false;
+  };
 
-    const onClickTwo = () => {
-        checkbox3.current!.checked = false;
-        checkbox1.current!.checked = false;
-        setSafety(safety === "no" ? "" : "no");
-        if (checkbox2.current!.checked) {
-            toggleIsOpen();
-        }
-    };
-    const onClickThree = () => {
-        checkbox1.current!.checked = false;
-        checkbox2.current!.checked = false;
-        setSafety(safety === "unsure" ? "" : "unsure");
-        if (checkbox3.current!.checked) {
-            toggleIsOpen();
-        }
-    };
+  const onClickTwo = () => {
+    checkbox3.current!.checked = false;
+    checkbox1.current!.checked = false;
+    setSafety(safety === "yes" ? "" : "yes");
+    if (checkbox2.current!.checked) {
+      toggleIsOpen();
+    }
+  };
 
-    const toggleIsOpen = () => {
-        isOpen ? setIsOpen(false) : setIsOpen(true);
-    };
+  const onClickThree = () => {
+    checkbox2.current!.checked = false;
+    checkbox1.current!.checked = false;
+    setSafety(safety === "unsure" ? "" : "unsure");
+    if (checkbox3.current!.checked) {
+      toggleIsOpen();
+    }
+  };
 
-    return (
-        <>
-            <div className="card-content">
-                <p className="py-2">Are you safe right now?</p>
-                <Checkbox
-                    className="checkbox"
-                    ref={checkbox1}
-                    label="Yes"
-                    onChange={onClickOne}
-                />
-                <Checkbox
-                    className="checkbox px-2"
-                    label="No"
-                    ref={checkbox2}
-                    onChange={onClickTwo}
-                />
-                <Checkbox
-                    className="checkbox px-2 "
-                    label="Not sure"
-                    ref={checkbox3}
-                    onChange={onClickThree}
-                />
-            </div>
+  const toggleIsOpen = () => {
+    isOpen ? setIsOpen(false) : setIsOpen(true);
+  };
 
-            {isOpen && <SafetyPopup onClick={toggleIsOpen} safety={safety} />}
-        </>
-    );
+  return (
+    <>
+      <div className="card-content">
+        <p className="py-2">Are you in physical danger?</p>
+        <Checkbox
+          className="checkbox px-2"
+          label="No"
+          ref={checkbox1}
+          onChange={onClickOne}
+        />
+        <Checkbox
+          className="checkbox"
+          ref={checkbox2}
+          label="Yes"
+          onChange={onClickTwo}
+        />
+
+        <Checkbox
+          className="checkbox px-2 "
+          label="Not sure"
+          ref={checkbox3}
+          onChange={onClickThree}
+        />
+      </div>
+
+      {isOpen && <SafetyPopup onClick={toggleIsOpen} safety={safety} />}
+    </>
+  );
 };
 
 export default SafetyCheck;
--- components/SafetyPopup.tsx
+++ components/SafetyPopup.tsx
@@ -5,77 +5,61 @@ import Image from "next/image";
 import Link from "next/link";
 
 interface Props {
-    safety: string;
-    onClick: (event: React.MouseEvent<HTMLInputElement>) => void;
+  safety: string;
+  onClick: (event: React.MouseEvent<HTMLInputElement>) => void;
 }
 
 const SafetyPopup = ({ safety, onClick }: Props): ReactElement => {
-    const [isOpen, setIsOpen] = useState<boolean>(true);
+  const [isOpen, setIsOpen] = useState<boolean>(true);
 
-    const handleCloseBt = () => {
-        setIsOpen(false);
-    };
+  const handleCloseBt = () => {
+    setIsOpen(false);
+  };
 
-    return (
-        <>
-            {isOpen && (
-                <div className={styles.popup}>
-                    <div className={styles.close_bt} onClick={onClick}>
-                        <p>X</p>
-                    </div>
-                    {safety === "no" && (
-                        <>
-                            <p className="block">Do you need the police?</p>
-                            <div className={styles.call_bt}>
-                                <Link href="tel:110" passHref>
-                                    <Image
-                                        src={phone}
-                                        alt="phone"
-                                        width="25"
-                                        height="25"
-                                    />
-                                </Link>
-                            </div>
-                            <p className="block">Do you need an ambulance?</p>
-                            <div className={styles.call_bt}>
-                                <Link href="tel:112" passHref>
-                                    <Image
-                                        src={phone}
-                                        alt="phone"
-                                        width="25"
-                                        height="25"
-                                    />
-                                </Link>
-                            </div>
-                            <p className="block is-size-7">
-                                If you don’t know what to say, tell them your
-                                location first and then what has happened.{" "}
-                            </p>
-                        </>
-                    )}
-                    {(safety === "unsure" || safety === "no") && (
-                        <>
-                            <p className="block is-size-7">
-                                If you’d like to talk to somebody or just not be
-                                alone right now, you can call the national
-                                helpline.
-                            </p>
-                            <div className={styles.call_bt}>
-                                <Link href="tel:08000116116" passHref>
-                                    <Image
-                                        src={phone}
-                                        alt="phone"
-                                        width="25"
-                                        height="25"
-                                    />
-                                </Link>
-                            </div>
-                        </>
-                    )}
-                </div>
-            )}
-        </>
-    );
+  return (
+    <>
+      {isOpen && (
+        <div className={styles.popup}>
+          <div className={styles.close_bt} onClick={onClick}>
+            <p>X</p>
+          </div>
+          {safety === "yes" && (
+            <>
+              <p className="block">Do you need an ambulance?</p>
+              <div className={styles.call_bt}>
+                <Link href="tel:112" passHref>
+                  <Image src={phone} alt="phone" width="25" height="25" />
+                </Link>
+              </div>
+              <p className="block">Do you need the police?</p>
+              <div className={styles.call_bt}>
+                <Link href="tel:110" passHref>
+                  <Image src={phone} alt="phone" width="25" height="25" />
+                </Link>
+              </div>
+              <p className="block is-size-7">
+                If you don’t know what to say, tell them your location first and
+                then what has happened.{" "}
+              </p>
+            </>
+          )}
+          {(safety === "unsure" || safety === "yes") && (
+            <>
+              <p className="block is-size-7">
+                If you’d like to talk to somebody or just not be alone right
+                now, you can call the national helpline.
+              </p>
+              <div className={styles.call_bt}>
+                <Link href="tel:08000116116" passHref>
+                  <Image src={phone} alt="phone" width="25" height="25" />
+                </Link>
+              </div>
+            </>
+          )}
+        </div>
+      )}
+    </>
+  );
 };
 
 export default SafetyPopup;
