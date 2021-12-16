6b8a6cf6b0d40c239e77f043e85f501052f7fe0d
Nadav Kirsh <saturdaysyouth@gmail.com>
2021-12-16 17:36:01 +0100

    //

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ import Link from "next/link";
 
 import en from "../locales/en";
 import de from "../locales/de";
+import SafetyPopup from "../components/SafetyPopup";
 
 const Home: NextPage = () => {
   const router = useRouter();
@@ -28,41 +29,40 @@ const Home: NextPage = () => {
           <option value="de">DE</option>
         </select>
       </div>
-      <div className="block">
-        <div className="block py-2">
-          <p>{t.test}</p>
-          <p>Hi, my name is Lou,</p>
+
+      <div className="block py-2">
+        <p>{t.test}</p>
+        <p>Hi, my name is Lou,</p>
+        <br />
+        <p>
+          I’m the first digital companion for people affected by gender-based
+          violence. Whatever you might go through, I am here to support you on
+          your journey towards a self-determined life.
           <br />
-          <p>
-            I’m the first digital companion for people affected by gender-based
-            violence. Whatever you might go through, I am here to support you on
-            your journey towards a self-determined life.
-            <br />
-            <br />
-            Tell me a little about what your situation and I’ll show you what
-            might be helpful for you.{" "}
-          </p>
-        </div>
-        <GetHelp />
-        <p className="block pt-5">
-          Learn more about
-          <Link href="/sexual_general" passHref>
-            {" sexual"}
-          </Link>
-          {" or"}
-          <Link href="/domestic_general" passHref>
-            {" domestic"}
-          </Link>
-          {""} violence.
-        </p>
-        <p className="block">
-          You’d like to know more about me?
-          <br /> Let me <a>introduce myself to you.</a>
-          <br /> And if you’d like to stay in touch, sign up to my newsletter{" "}
-          <a>here</a>. One thing is for sure: together,{" "}
-          <a>we can change a lot.</a>
+          <br />
+          Tell me a little about what your situation and I’ll show you what
+          might be helpful for you.{" "}
         </p>
       </div>
+      <GetHelp />
+      <p className="block pt-5">
+        Learn more about
+        <Link href="/sexual_general" passHref>
+          {" sexual"}
+        </Link>
+        {" or"}
+        <Link href="/domestic_general" passHref>
+          {" domestic"}
+        </Link>
+        {""} violence.
+      </p>
+      <p className="block">
+        You’d like to know more about me?
+        <br /> Let me <a>introduce myself to you.</a>
+        <br /> And if you’d like to stay in touch, sign up to my newsletter{" "}
+        <a>here</a>. One thing is for sure: together,{" "}
+        <a>we can change a lot.</a>
+      </p>
     </>
   );
 };
