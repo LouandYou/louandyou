e6b5b507b63bc6dbaa5dc6cf5612e7b56bc3c5ab
Nadav Kirsh <saturdaysyouth@gmail.com>
2022-01-26 13:45:13 +0200

    animate dropdown button

--- src/components/static/Dropdown.tsx
+++ src/components/static/Dropdown.tsx
@@ -9,14 +9,25 @@ interface IProps {
 
 export function Dropdown(props: IProps): ReactElement {
   const [isVisible, setIsVisible] = useState<boolean>(false);
+  const [rotate, setRotate] = useState<boolean>(false);
+
+  const handleClick = () => {
+    if (rotate) {
+      setRotate(false);
+      setIsVisible(!isVisible);
+    } else {
+      setIsVisible(!isVisible);
+      setRotate(true);
+    }
+  };
 
   return (
     <section className={styles.section} data-dark-bg="false" id={props.id}>
       <div className={styles.dropdown_wrapper}>
         <p className={styles.label}>{props.label}</p>
         <button
-          onClick={() => setIsVisible(!isVisible)}
-          className={styles.dropdown_button}
+          onClick={handleClick}
+          className={`${styles.dropdown_button} ${rotate ? styles.rotate : ""}`}
         ></button>
       </div>
       {isVisible && <div className={styles.content}>{props.content}</div>}
