d15f8a1030628ee017a9bf3c4567d3d9ac749c11
Nadav Kirsh <saturdaysyouth@gmail.com>
2022-02-15 16:54:29 +0100

    make button appear on second section

--- src/components/static/ExitButton/ExitButton.tsx
+++ src/components/static/ExitButton/ExitButton.tsx
@@ -1,9 +1,16 @@
 import React, { ReactElement, useContext } from "react";
+import useScrollingBackgroundColor from "../../../utils/useScrollingBackgroundColor";
 import styles from "./ExitButton.module.scss";
 import { ExitButtonContext } from "./ExitButtonContext";
 
 export function ExitButton(): ReactElement {
   const { isVisible } = useContext(ExitButtonContext);
+
+  const isLandingPage = useScrollingBackgroundColor({
+    elements: () =>
+      Array.from(document.querySelectorAll("#section_1, #section_2")),
+    offset: 70,
+  });
 
   const onClick = () => {
     window.location.replace("https://www.google.de/");
@@ -11,7 +18,7 @@ export function ExitButton(): ReactElement {
 
   return (
     <>
-      {isVisible && (
+      {isVisible && !isLandingPage && (
         <button onClick={onClick} className={styles.exit_bt}>
           ✕
         </button>
