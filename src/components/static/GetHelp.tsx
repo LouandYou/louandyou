8836e38cd09967613508e5dcfe0dd978c3681cb0
Nadav Kirsh <saturdaysyouth@gmail.com>
2022-02-28 15:08:56 +0200

    Cleanup 🧹

--- src/components/static/GetHelp.tsx
+++ src/components/static/GetHelp.tsx
@@ -1,54 +1,25 @@
-import React, { ReactElement, useRef, useState } from "react";
+import React, { ReactElement, useState } from "react";
 import { Checkbox } from "./Checkbox";
 import styles from "./GetHelp.module.scss";
 import Link from "next/link";
 import { SafetyCheck } from "./SafetyCheck";
-import { Button } from ".";
 
 export function GetHelp(): ReactElement {
-  const [isSexual, setIsSexual] = useState<boolean>();
+  const [violanceType, setViolanceType] = useState<string>();
   const [isLongAgo, setIsLongAgo] = useState<boolean>();
 
-  const checkbox1 = useRef<HTMLInputElement>();
-  const checkbox2 = useRef<HTMLInputElement>();
-  const checkbox3 = useRef<HTMLInputElement>();
-
-  const onClickOne = () => {
-    checkbox3.current!.checked = false;
-    checkbox2.current!.checked = false;
-    if (checkbox1.current!.checked) {
-      setIsSexual(true);
-    }
-  };
-
-  const onClickTwo = () => {
-    checkbox3.current!.checked = false;
-    checkbox1.current!.checked = false;
-    if (checkbox2.current!.checked) {
-      setIsSexual(false);
-    }
-  };
-  const onClickThree = () => {
-    checkbox1.current!.checked = false;
-    checkbox2.current!.checked = false;
-    if (checkbox3.current!.checked) {
-      setIsSexual(false);
-    }
-  };
-
   const getHref = () => {
-    if (isSexual && !isLongAgo) {
+    if (violanceType === "sexual" && !isLongAgo) {
       return "/result_1";
-    } else if (isSexual && isLongAgo) {
+    } else if (violanceType === "sexual" && isLongAgo) {
       return "/result_2";
-    } else if (!isSexual && !isLongAgo) {
+    } else if (violanceType !== "sexual" && !isLongAgo) {
       return "/result_3";
     }
-
     return "/result_4";
   };
 
-  const canSubmit = () => isSexual !== undefined && isLongAgo !== undefined;
+  const canSubmit = () => violanceType !== undefined && isLongAgo !== undefined;
 
   return (
     <>
@@ -59,24 +30,32 @@ export function GetHelp(): ReactElement {
 
       <div className="pt-5 is-flex">
         <Checkbox
-          type="checkbox"
+          type="radio"
           label="sexual"
-          ref={checkbox1}
-          onChange={onClickOne}
+          checked={violanceType === "sexual"}
+          onChange={() => setViolanceType("sexual")}
         />
         <Checkbox
-          type="checkbox"
+          type="radio"
           label="domestic"
-          ref={checkbox2}
-          onChange={onClickTwo}
+          checked={violanceType === "domestic"}
+          onChange={() => setViolanceType("domestic")}
         />
       </div>
       <div className="pt-4">
         <Checkbox
-          type="checkbox"
+          type="radio"
+          label="both"
+          checked={violanceType === "both"}
+          onChange={() => setViolanceType("both")}
+        />
+      </div>
+      <div className="pt-4">
+        <Checkbox
+          type="radio"
           label="I’m not sure / I’d rather not say"
-          ref={checkbox3}
-          onChange={onClickThree}
+          checked={violanceType === "not sure"}
+          onChange={() => setViolanceType("not sure")}
         />
       </div>
       <h2 className={styles.headline}>how long ago did it happen?</h2>
@@ -100,7 +79,7 @@ export function GetHelp(): ReactElement {
         {canSubmit() && (
           <Link href={getHref()} passHref>
             <button className={`${styles.button} ${styles.purple}`}>
-              find out how I can help
+              get started
             </button>
           </Link>
         )}
