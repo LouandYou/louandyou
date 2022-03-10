12c7ad3dec0b01abdd6d2b75e88cca02bb21de8e
Nadav Kirsh <saturdaysyouth@gmail.com>
2022-03-10 15:09:20 +0200

    storyblock

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -1,8 +1,8 @@
-import Link from "next/link";
 import React, { useContext, useEffect, useState } from "react";
 import { Checkbox, Layout, Slider } from "../src/components/static";
 import { useStoryblok } from "../src/lib/storyblok";
 import { pageGetStaticProps } from "../src/lib/pageGetStaticProps";
+import { Text } from "../src/components/dynamic";
 import Cookies from "js-cookie";
 
 import styles from "./settings.module.scss";
@@ -11,17 +11,19 @@ import { useRouter } from "next/dist/client/router";
 import { ExitButtonContext } from "../src/components/static/ExitButton/ExitButtonContext";
 
 const Settings = ({ story, locale, preview, defaultLocale }) => {
-  story = useStoryblok(story, preview, locale);
+  const { content } = useStoryblok(story, preview, locale);
   const router = useRouter();
 
   const [fontSize, setFontSize] = useState<string>("normal");
   const [isCookies, setIsCookies] = useState<boolean>(true);
+  const [contrast, setContrast] = useState<string>("no contrast");
 
   const { toggleIsVisible } = useContext(ExitButtonContext);
 
   useEffect(() => {
     Cookies.get("FONT_BIG") ? setFontSize("big") : null;
     Cookies.get("DISABLE_COOKIES") ? setIsCookies(false) : null;
+    Cookies.get("CONTRAST") ? setContrast("contrast") : null;
   }, []);
 
   const handleLocale = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -31,20 +33,38 @@ const Settings = ({ story, locale, preview, defaultLocale }) => {
       : router.push("/en/settings");
   };
 
+  const handleContrast = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value === "no contrast") {
+      setContrast(e.target.value);
+      Cookies.remove("CONTRAST");
+      document.documentElement.style.setProperty("--color-font", "white");
+    } else {
+      document.documentElement.style.setProperty("--color-font", "#101223");
+      Cookies.set("CONTRAST", true);
+      setContrast(e.target.value);
+    }
+  };
+
   const handleFontSize = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.value === "normal") {
-      document.documentElement.style.setProperty("--size-font", "1em");
       document.documentElement.style.setProperty(
-        "--size-font-checkbox",
-        "12px"
+        "--size-font-paragraph",
+        "17px"
+      );
+      document.documentElement.style.setProperty(
+        "--size-font-paragraph-title",
+        "18px"
       );
       setFontSize(e.target.value);
       Cookies.remove("FONT_BIG");
     } else {
-      document.documentElement.style.setProperty("--size-font", "1.25em");
       document.documentElement.style.setProperty(
-        "--size-font-checkbox",
-        "18px"
+        "--size-font-paragraph",
+        "24px"
+      );
+      document.documentElement.style.setProperty(
+        "--size-font-paragraph-title",
+        "25px"
       );
       setFontSize(e.target.value);
       isCookies && Cookies.set("FONT_BIG", true);
@@ -68,93 +88,76 @@ const Settings = ({ story, locale, preview, defaultLocale }) => {
   };
 
   return (
-    <Layout>
-      <section
-        data-dark-bg="false"
-        id="section_1"
-        className={styles.landing_page}
-      >
-        <h1 className={styles.title}>settings</h1>
+    <Layout content={story.content}>
+      <section className={styles.landing_page}>
+        <h1>{content.section1_title}</h1>
+        <p className="mb-5">{content.language}</p>
         <div className={styles.checkbox_wraper}>
-          <p>language</p>
-          <div>
-            <Checkbox
-              type="radio"
-              value="de"
-              checked={locale === "de"}
-              label="deutsch"
-              onChange={handleLocale}
-            />
-          </div>
-          <div>
-            <Checkbox
-              type="radio"
-              value="en"
-              checked={locale === "en"}
-              label="english"
-              onChange={handleLocale}
-            />
-          </div>
+          <Checkbox
+            type="radio"
+            value="de"
+            checked={locale === "de"}
+            label="deutsch"
+            onChange={handleLocale}
+          />
+          <Checkbox
+            type="radio"
+            value="en"
+            checked={locale === "en"}
+            label="english"
+            onChange={handleLocale}
+          />
         </div>
-
+        <p className="mb-5">{content.contrast}</p>
         <div className={styles.checkbox_wraper}>
-          <p>font size</p>
-          <div>
-            <Checkbox
-              type="radio"
-              checked={fontSize === "normal"}
-              label="normal"
-              value="normal"
-              onChange={handleFontSize}
-            />
-          </div>
-          <div>
-            <Checkbox
-              type="radio"
-              checked={fontSize === "big"}
-              label="big"
-              value="big"
-              onChange={handleFontSize}
-            />
-          </div>
+          <Checkbox
+            type="radio"
+            value="no contrast"
+            checked={contrast === "no contrast"}
+            label={content.no_contrast}
+            onChange={handleContrast}
+          />
+          <Checkbox
+            type="radio"
+            value="contrast"
+            checked={contrast === "contrast"}
+            label={content.contrast}
+            onChange={handleContrast}
+          />
+        </div>
+        <p className="mb-5">{content.font_size}</p>
+        <div className={styles.checkbox_wraper}>
+          <Checkbox
+            type="radio"
+            checked={fontSize === "normal"}
+            label="normal"
+            value="normal"
+            onChange={handleFontSize}
+          />
+          <Checkbox
+            type="radio"
+            checked={fontSize === "big"}
+            label="big"
+            value={content.big}
+            onChange={handleFontSize}
+          />
         </div>
       </section>
 
-      <section data-dark-bg="true" id="section_2" className={styles.color_page}>
-        <h2 className={styles.title_white}>
-          i have created an optional exit button for you
-        </h2>
-        <p>
-          The exit button down below is super helpful in all kinds of
-          situations. Wether it’s your partner who mustn’t know you’re here to
-          your coworker coming in the room or a parent who’d worry too much if
-          they saw Lou&You. <br />
-          <br /> If you know you’re only here when you’re on your own and you’re
-          safe, here you can permanently remove the exit button. This only works
-          with cookies enabled, so make sure to check them first down below.
-        </p>
-        <button className={styles.exit_btn}>✕</button>
+      <section className={styles.color_page}>
+        <h1>{content.section2_title}</h1>
+        <Text blok={content} attribute={"section2_p"} />
         <div className={styles.switch_wraper1}>
-          <p className={styles.white_lable}>disable exit button permanently</p>
+          <p className={styles.white_lable}>exit button</p>
           <Slider onChange={handleExitButton} blackBorder={false} />
         </div>
       </section>
-      <section
-        data-dark-bg="false"
-        id="section_3"
-        className={styles.landing_page}
-      >
-        <h2 className={styles.title_black}>cookies & privacy</h2>
-        <p>
-          <b>Warning:</b> If this device is not safe you should not allow any
-          cookies.
+      <section className={styles.landing_page}>
+        <h1>{content.section3_title}</h1>
+        <p className="pb-4">
+          <b>{content.section3_subtitle1}</b>
         </p>
-        <p>
-          <b>essential cookies</b>
-          <br />
-          These cookies save your preferences regarding language, contrast, font
-          size and the exit button
-        </p>
+        <Text blok={content} attribute={"section3_p1"} />
         <div className={styles.switch_wraper2}>
           <Slider
             onChange={handleCookies}
@@ -162,19 +165,12 @@ const Settings = ({ story, locale, preview, defaultLocale }) => {
             blackBorder={true}
           />
         </div>
-        <p>
-          <b>share usage data</b>
-          <br />I want my data to be shared anonymously with LouAndYou and
-          researchers, so that the site can be improved and we can learn about
-          helping victims of domestic violence
+        <p className="pb-4">
+          <b>{content.section3_suptitle2}</b>
         </p>
+        <p>{content.section3_p2}</p>
         <div className={styles.switch_wraper2}>
           <Slider blackBorder={true} />
-        </div>
-        <div className={styles.button_wraper}>
-          <button className={styles.button}>
-            <Link href={"/"}>back to previous page</Link>
-          </button>
         </div>
       </section>
       <Footer />
@@ -185,7 +181,7 @@ const Settings = ({ story, locale, preview, defaultLocale }) => {
 export async function getStaticProps(props) {
   return pageGetStaticProps({
     ...props,
-    slug: "home",
+    slug: "settings",
   });
 }
 
