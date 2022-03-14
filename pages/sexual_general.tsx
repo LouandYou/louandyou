ef7b9390504cb338003b6dc38e188274e9c7ccab
Nadav Kirsh <saturdaysyouth@gmail.com>
2022-03-14 15:17:49 +0200

    update page

--- pages/sexual_general.tsx
+++ pages/sexual_general.tsx
@@ -1,153 +1,64 @@
-import { NextPage } from "next";
-import Link from "next/link";
-import { DropdownBlack, Layout } from "../src/components/static";
-import { Footer } from "../src/components/static/Footer";
-
-import styles from "./general.module.scss";
+import { LinkButton } from "../src/components/static";
+import styles from "./result.module.scss";
 import { pageGetStaticProps } from "../src/lib/pageGetStaticProps";
+import { PageContent, Text } from "../src/components/dynamic";
+import { Feedback } from "../src/components/static/Popups/Feedback";
+import { useState } from "react";
 
-const SexualGeneral: NextPage = () => {
+export default function Page({ story }) {
+  const [isFeedbackOpen, setIsFeedbackOpen] = useState<boolean>(false);
+  const { content } = story;
   return (
     <>
-      <section
-        data-dark-bg="true"
-        id="section_1"
-        className={styles.landing_page}
-      >
-        <h1 className={styles.main_title}>
-          what’s good to know about sexual violence
-        </h1>
-      </section>
-      <section
-        data-dark-bg="false"
-        id="section_2"
-        className={styles.white_page}
-      >
-        <p className={`${styles.description} ${styles.red}`}>
-          knowing if you or a loved one is experiencing violence in their home
-          or relationship isn’t as easy as it might seem.
-          <br />
-          <br />
-          while some cases are obvious, you’ll most likely have more questions
-          than answers in the beginning. <br />
-          <br />
-          so to find out, here’s a little guide with what’s important to know
-          about.{" "}
+      <section className={styles.landing_page}>
+        <h1>{content.headline}</h1>
+        <p className="pt-3">
+          <Text blok={content} attribute={"subline"} />
         </p>
       </section>
-      <section data-dark-bg="true" id="section_3" className={styles.color_page}>
-        <h2 className={styles.subtitle}>was ist sexualisierte Gewalt?</h2>
+      <PageContent blok={content} name={"body"} />;
+      <section className={styles.warning} id="section_14">
+        <h1 className="mb-5">{content.warning_title}</h1>
+        <p id="trigger-warning">{content.warning_p}</p>
+        <div className="is-flex is-justify-content-center">
+          <LinkButton variant={`white`} href={"#section_17"}>
+            {content.warning_button}
+          </LinkButton>
+        </div>
       </section>
-      <section
-        data-dark-bg="false"
-        id="section_4"
-        className={styles.white_page}
-      >
-        <DropdownBlack
-          label={
-            "A sentence describing what kind of options there are and for whom etc. A sentence describing what kind of options there are and for whom etc."
-          }
-          content={
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
-          }
-        />
+      ;
+      <PageContent blok={content} name={"body2"} />;
+      <section className={styles.color_page}>
+        <h1 className="mb-5">{content.where_to_title}</h1>
+        <Text blok={content} attribute={"where_to_p"} />
       </section>
-      <section data-dark-bg="true" id="section_5" className={styles.color_page}>
-        <h2 className={styles.subtitle}>welche Formen gibt es?</h2>
-      </section>
-      <section
-        data-dark-bg="false"
-        id="section_6"
-        className={styles.white_page}
-      >
-        <DropdownBlack
-          label={
-            "A sentence describing what kind of options there are and for whom etc. A sentence describing what kind of options there are and for whom etc."
-          }
-          content={
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
-          }
-        />
-      </section>
-      <section data-dark-bg="true" id="section_7" className={styles.color_page}>
-        <h2 className={styles.subtitle}>was sollte ich jetzt tun?</h2>
-      </section>
-      <section
-        data-dark-bg="false"
-        id="section_8"
-        className={styles.white_page}
-      >
-        <DropdownBlack
-          label={
-            "A sentence describing what kind of options there are and for whom etc. A sentence describing what kind of options there are and for whom etc."
-          }
-          content={
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
-          }
-        />
-      </section>
-      <section data-dark-bg="true" id="section_9" className={styles.color_page}>
-        <h2 className={styles.subtitle}>welche Rechte habe ich?</h2>
-      </section>
-      <section
-        data-dark-bg="false"
-        id="section_10"
-        className={styles.white_page}
-      >
-        <DropdownBlack
-          label={
-            "A sentence describing what kind of options there are and for whom etc. A sentence describing what kind of options there are and for whom etc."
-          }
-          content={
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
-          }
-        />
-      </section>
-      <section
-        data-dark-bg="true"
-        id="section_11"
-        className={styles.color_page}
-      >
-        <h2 className={styles.subtitle}>wie kann ich mir selber helfen?</h2>
-      </section>
-      <section
-        data-dark-bg="false"
-        id="section_12"
-        className={styles.white_page}
-      >
-        <DropdownBlack
-          label={
-            "A sentence describing what kind of options there are and for whom etc. A sentence describing what kind of options there are and for whom etc."
-          }
-          content={
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
-          }
-        />
-      </section>
-      <section
-        data-dark-bg="true"
-        id="section_13"
-        className={styles.color_page_last}
-      >
-        <h1 className={styles.subtitle}>
-          would you like more information related to your personal situation?
-        </h1>
-        <button className={styles.white_btn}>
-          <Link href="/">yes, that might help</Link>
-        </button>
+      ;
+      <section className={styles.white_page}>
+        <h2>{content.lets_build_title1}</h2>
+        <p className="mb-5">{content.lets_build_p3}</p>
+        <Text blok={content} attribute={"lets_build_p1"} />
+        <h3>{content.lets_build_title2}</h3>
+        <Text blok={content} attribute={"lets_build_p2"} />
+
+        <div className="is-flex is-justify-content-center">
+          <button
+            onClick={() => setIsFeedbackOpen(!isFeedbackOpen)}
+            className={`${styles.button} ${styles.purple}`}
+          >
+            give feedback
+          </button>
+        </div>
+        {isFeedbackOpen && (
+          <Feedback onClose={() => setIsFeedbackOpen(!isFeedbackOpen)} />
+        )}
       </section>
     </>
   );
-};
-
+}
 
 export async function getStaticProps(props) {
   return pageGetStaticProps({
     ...props,
-    // TODO change
-    slug: "result_1"
+    slug: "sexual_general",
   });
 }
-
-
-export default SexualGeneral;
