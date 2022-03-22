649c15d5e730f9d059687703dfec79f31be2b17c
Nadav Kirsh <saturdaysyouth@gmail.com>
2022-03-22 14:42:45 +0100

    bolbs

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,7 @@ import Image from "next/image";
 import styles from "./index.module.scss";
 import { Text } from "../src/components/dynamic";
 import { pageGetStaticProps } from "../src/lib/pageGetStaticProps";
+import Blobs from "../src/components/static/Popups/Blobs";
 
 export default function Page({
   story,
@@ -27,17 +28,18 @@ export default function Page({
           id="section_1"
           className={styles.gradient_page}
         >
-          <Image
-            src={"/logo_full_white.svg"}
-            layout="fixed"
-            width="250"
-            height="100"
-            alt="logo"
-            priority
-          />
-          <Text blok={content} attribute={"description"} />
-
-          <div className={styles.language_wrapper}>
+          <div style={{ zIndex: 2 }}>
+            <Image
+              src={"/logo_full_white.svg"}
+              layout="fixed"
+              width="250"
+              height="100"
+              alt="logo"
+              priority
+            />
+            <Text blok={content} attribute={"description"} />
+          </div>
+          <div className={`is-hidden-tablet ${styles.language_wrapper}`}>
             <div className={styles.language_switch}>
               {locales
                 .map((loc) => (
@@ -66,6 +68,7 @@ export default function Page({
             story={story}
             preview={preview}
           />
+          <Blobs />
         </section>
       </div>
       <div className={styles.page_wrapper}>
@@ -75,15 +78,18 @@ export default function Page({
         </section>
 
         <section className={styles.gradient_page}>
-          <h1>{content.title1}</h1>
-          <div className={styles.btn_container}>
-            <LinkButton href="/domestic_general" variant="white">
-              {content.domestic_violence}
-            </LinkButton>
-            <LinkButton href="/sexual_general" variant="white">
-              {content.sexual_violence}
-            </LinkButton>
+          <div style={{ zIndex: 2 }}>
+            <h1>{content.title1}</h1>
+            <div className={styles.btn_container}>
+              <LinkButton href="/domestic_general" variant="white">
+                {content.domestic_violence}
+              </LinkButton>
+              <LinkButton href="/sexual_general" variant="white">
+                {content.sexual_violence}
+              </LinkButton>
+            </div>
           </div>
+          <Blobs />
         </section>
 
         <section className={styles.white_page}>
@@ -91,15 +97,18 @@ export default function Page({
           <p>{content.title2_p}</p>
         </section>
         <section className={styles.gradient_page}>
-          <h1>{content.title3}</h1>
-          <div className={styles.btn_container}>
-            <LinkButton
-              href={"https://seu2.cleverreach.com/f/314808-318550/"}
-              variant="white"
-            >
-              {content.newsletter}
-            </LinkButton>
+          <div style={{ zIndex: 2 }}>
+            <h1>{content.title3}</h1>
+            <div className={styles.btn_container}>
+              <LinkButton
+                href={"https://seu2.cleverreach.com/f/314808-318550/"}
+                variant="white"
+              >
+                {content.newsletter}
+              </LinkButton>
+            </div>
           </div>
+          <Blobs />
         </section>
       </div>
     </>
