5865f0b259fa54c73937978c848a45def8e981a4
Nadav Kirsh <saturdaysyouth@gmail.com>
2022-04-12 12:52:21 +0200

    send feedback to info@louandyou.org

--- pages/api/sendgrid.js
+++ pages/api/sendgrid.js
@@ -5,12 +5,12 @@ function sendEmail(req, res) {
   console.debug("REQ.BODY", req.body);
   mail
     .send({
-      to: "saturdaysyouth@gmail.com",
-      from: "saturdaysyouth@gmail.com",
+      to: "info@louandyou.org",
+      from: "nk@louandyou.org",
       subject: "User Feedback",
-      html: `<p>How helpful have these information been? <bold>${req.body.rating}</bold></p>
-      <p>Did I address your personal needs this time? <bold>${req.body.checkboxValue}</bold></p>
-      <p>What else would you like me to know or to help you with in the future? <bold>${req.body.textareaValue}</bold></p>
+      html: `<p>How helpful have these information been? <b>${req.body.rating}</b></p>
+      <p>Did I address your personal needs this time? <b>${req.body.checkboxValue}</b></p>
+      <p>What else would you like me to know or to help you with in the future? <b>${req.body.textareaValue}</b></p>
       `,
     })
     .then(() => {
