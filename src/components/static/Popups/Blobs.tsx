b15a7b270a30c9d84a73644c84c5126421603a2d
Nadav Kirsh <saturdaysyouth@gmail.com>
2022-04-21 16:20:20 +0200

    blobs optimization

--- src/components/static/Popups/Blobs.tsx
+++ src/components/static/Popups/Blobs.tsx
@@ -5,143 +5,83 @@ function Blobs() {
   return (
     <div className={styles.blobs_container}>
       <div className={styles.blob_centercenter_indigo_div}>
-        <div
-          className={styles.blob_centercenter_indigo_img}
-          style={{ backgroundImage: "url(https://i.imgur.com/SyldkoW.png" }}
-        ></div>
+        <div className={styles.blob_centercenter_indigo_img}></div>
       </div>
 
       <div className={styles.blob_upper_left_spinning_light_green_div}>
-        <div
-          className={styles.blob_upper_left_spinning_light_green_img}
-          style={{ backgroundImage: "url(https://i.imgur.com/cS1lars.png" }}
-        ></div>
+        <div className={styles.blob_upper_left_spinning_light_green_img}></div>
       </div>
 
       <div className={styles.blob_left_middle_purple_div}>
-        <div
-          className={styles.blob_left_middle_purple_img}
-          style={{ backgroundImage: "url(https://i.imgur.com/K6BT7qx.png" }}
-        ></div>
+        <div className={styles.blob_left_middle_purple_img}></div>
       </div>
 
       <div className={styles.blob_left_lower_middle_purple_div}>
-        <div
-          className={styles.blob_left_lower_middle_purple_img}
-          style={{ backgroundImage: "url(https://i.imgur.com/Lr1e6W2.png" }}
-        ></div>
+        <div className={styles.blob_left_lower_middle_purple_img}></div>
       </div>
 
       <div className={styles.blob_upper_left_spinning_dark_green_div}>
-        <div
-          className={styles.blob_upper_left_spinning_dark_green_img}
-          style={{ backgroundImage: "url(https://i.imgur.com/eKuPd6k.png" }}
-        ></div>
+        <div className={styles.blob_upper_left_spinning_dark_green_img}></div>
       </div>
 
       <div className={styles.blob_upper_left_spinning_indigo_div}>
-        <div
-          className={styles.blob_upper_left_spinning_indigo_img}
-          style={{ backgroundImage: "url(https://i.imgur.com/tfuIJ5j.png" }}
-        ></div>
+        <div className={styles.blob_upper_left_spinning_indigo_img}></div>
       </div>
 
       <div className={styles.blob_upper_left_fixed_purple_div}>
-        <div
-          className={styles.blob_upper_left_fixed_purple_img}
-          style={{ backgroundImage: "url(https://i.imgur.com/CtlfnBk.png" }}
-        ></div>
+        <div className={styles.blob_upper_left_fixed_purple_img}></div>
       </div>
 
       <div className={styles.blob_LARGE_lower_light_green_div}>
-        <div
-          className={styles.blob_LARGE_lower_light_green_img}
-          style={{ backgroundImage: "url(https://i.imgur.com/ArNRR2v.png" }}
-        ></div>
+        <div className={styles.blob_LARGE_lower_light_green_img}></div>
       </div>
 
       <div className={styles.blob_LARGE_upper_right_purple_div}>
-        <div
-          className={styles.blob_LARGE_upper_right_purple_img}
-          style={{ backgroundImage: "url(https://i.imgur.com/K6BT7qx.png" }}
-        ></div>
+        <div className={styles.blob_LARGE_upper_right_purple_img}></div>
       </div>
 
       <div className={styles.blob_LARGE_lower_dark_green_div}>
-        <div
-          className={styles.blob_LARGE_lower_dark_green_img}
-          style={{ backgroundImage: "url(https://i.imgur.com/Ijyzqpm.png" }}
-        ></div>
+        <div className={styles.blob_LARGE_lower_dark_green_img}></div>
       </div>
 
       <div className={styles.blob_upper_middle_dark_green_fade_div}>
-        <div
-          className={styles.blob_upper_middle_dark_green_fade_img}
-          style={{ backgroundImage: "url(https://i.imgur.com/FgernT7.png" }}
-        ></div>
+        <div className={styles.blob_upper_middle_dark_green_fade_img}></div>
       </div>
 
       <div className={styles.blob_upper_middle_light_green_div}>
-        <div
-          className={styles.blob_upper_middle_light_green_img}
-          style={{ backgroundImage: "url(https://i.imgur.com/ijbwJrp.png" }}
-        ></div>
+        <div className={styles.blob_upper_middle_light_green_img}></div>
       </div>
 
       <div className={styles.blob_upper_right_div}>
-        <div
-          className={styles.blob_upper_right_indigo_img}
-          style={{ backgroundImage: "url(https://i.imgur.com/sUQRmIY.png" }}
-        ></div>
+        <div className={styles.blob_upper_right_indigo_img}></div>
       </div>
 
       <div className={styles.blob_middle_right_indigo_div}>
-        <div
-          className={styles.blob_middle_right_indigo_img}
-          style={{ backgroundImage: "url(https://i.imgur.com/lCadoFz.png" }}
-        ></div>
+        <div className={styles.blob_middle_right_indigo_img}></div>
       </div>
 
       <div className={styles.blob_lower_right_div}>
-        <div
-          className={styles.blob_lower_right_indigo_img}
-          style={{ backgroundImage: "url(https://i.imgur.com/pfdzPaN.png" }}
-        ></div>
+        <div className={styles.blob_lower_right_indigo_img}></div>
       </div>
 
       <div className={styles.blob_middle_right_dark_green_div}>
-        <div
-          className={styles.blob_middle_right_dark_green_img}
-          style={{ backgroundImage: "url(https://i.imgur.com/7OwFdDj.png" }}
-        ></div>
+        <div className={styles.blob_middle_right_dark_green_img}></div>
       </div>
 
       <div className={styles.blob_middle_right_light_green_div}>
-        <div
-          className={styles.blob_middle_right_light_green_img}
-          style={{ backgroundImage: "url(https://i.imgur.com/8RNV32R.png" }}
-        ></div>
+        <div className={styles.blob_middle_right_light_green_img}></div>
       </div>
 
       <div className={styles.fade_upper_right_div}>
-        <div
-          className={styles.fade_upper_right_yellow_img}
-          style={{ backgroundImage: "url(https://i.imgur.com/efa8oSl.png" }}
-        ></div>
+        <div className={styles.fade_upper_right_yellow_img}></div>
       </div>
 
       <div className={styles.fade_lower_middle_yellow_div}>
-        <div
-          className={styles.fade_lower_middle_yellow_img}
-          style={{ backgroundImage: "url(https://i.imgur.com/efa8oSl.png" }}
-        ></div>
+        <div className={styles.fade_lower_middle_yellow_img}></div>
       </div>
 
       <div className={styles.fade_left_yellow_div}>
-        <div
-          className={styles.fade_left_yellow_img}
-          style={{ backgroundImage: "url(https://i.imgur.com/tjHZseg.png" }}
-        ></div>
+        <div className={styles.fade_left_yellow_img}></div>
       </div>
     </div>
   );
