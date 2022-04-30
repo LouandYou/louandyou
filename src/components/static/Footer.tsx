3a1c8c2fa4a1f3978c85fcc5524df7a1d4504623
Nadav Kirsh <saturdaysyouth@gmail.com>
2022-04-30 09:44:21 +0200

    update links

--- src/components/static/Footer.tsx
+++ src/components/static/Footer.tsx
@@ -225,11 +225,19 @@ export function Footer({ content }): ReactElement {
             style={{ flex: "0.8" }}
             className="is-flex is-align-items-center is-justify-content-space-between"
           >
-            <Link href="/">{content.imprint}</Link>
-            <Link href="/">{content.data}</Link>
-            <Link href="/">{content.bylaws}</Link>
+            <Link href="//https://www.louandyou.org/imprint">
+              {content.imprint}
+            </Link>
+            <Link href="/https://www.louandyou.org/datenschutzerkl%C3%A4rung">
+              {content.data}
+            </Link>
+            <Link href="/https://www.louandyou.org/vereinssatzung">
+              {content.bylaws}
+            </Link>
             <Link href="/">{content.accessibility}</Link>
-            <Link href="/">{content.disclaimer}</Link>
+            <Link href="/https://www.louandyou.org/haftungsausschluss">
+              {content.disclaimer}
+            </Link>
           </div>
         </div>
       </div>
