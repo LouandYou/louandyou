ee42395216e99528d0b0df2cb229866b5b0cb19f
Nadav Kirsh <saturdaysyouth@gmail.com>
2022-06-23 11:44:37 +0200

    style update

--- src/components/dynamic/Text.tsx
+++ src/components/dynamic/Text.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { sbEditable } from "@storyblok/storyblok-editable";
 import { render } from "storyblok-rich-text-react-renderer";
+import styles from "./PageSection.module.scss";
 
 export const Text = ({ blok, attribute = "body" }) => {
   return (
-    <div {...sbEditable(blok)} key={blok.uuid || blok._uid}>
+    <div
+      {...sbEditable(blok)}
+      key={blok.uuid || blok._uid}
+      className={styles.text}
+    >
       {render(blok[attribute])}
     </div>
   );
