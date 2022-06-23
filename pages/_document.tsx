87e9c5251f0da11953cc6eb19a7914012c8296eb
Nadav Kirsh <saturdaysyouth@gmail.com>
2022-06-23 11:44:20 +0200

    change to tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,14 +7,8 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="true"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Maitree&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Maitree&display=optional"
             rel="stylesheet"
           />
           {/* Global Site Tag (gtag.js) */}
