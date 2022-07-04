50f18fc6409ba16fad22b10905cf4023cba2dd2f
Nadav Kirsh <saturdaysyouth@gmail.com>
2022-07-04 10:51:33 +0300

    impact page

---
+++ pages/about/impact.tsx
@@ -0,0 +1,28 @@
+import { pageGetStaticProps } from "../../src/lib/pageGetStaticProps";
+import { Text } from "../../src/components/dynamic";
+import styles from "./About.module.scss";
+
+export default function Page({ story }) {
+  const { content } = story;
+
+  return (
+    <>
+      <section className={styles.gradient_page}>
+        <Text blok={content} attribute={"text1"} />
+      </section>
+      <section className={styles.white_page}>
+        <Text blok={content} attribute={"text2"} />
+      </section>
+      <section className={styles.gradient_page}>
+        <Text blok={content} attribute={"text3"} />
+      </section>
+    </>
+  );
+}
+
+export async function getStaticProps(props) {
+  return pageGetStaticProps({
+    ...props,
+    slug: "about/impact",
+  });
+}
