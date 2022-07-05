93360fbd0a6f3e27f40af6628d4d3828d46d3015
Nadav Kirsh <saturdaysyouth@gmail.com>
2022-07-05 12:11:33 +0300

    test page

---
+++ pages/about/test.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function test() {
+  return (
+    <div>
+      <h1 style={{ fontFamily: "Wulkan", fontSize: "64px" }}>
+        Is This Wulkan noraml?
+      </h1>
+      <h1
+        style={{ fontFamily: "Wulkan", fontSize: "64px", fontWeight: "bold" }}
+      >
+        Is This Wulkan bold?
+      </h1>
+    </div>
+  );
+}
+
+export default test;
