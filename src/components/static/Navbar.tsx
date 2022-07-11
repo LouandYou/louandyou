14ca09cf54eb876b468039759a8591a83684dfc9
Nadav Kirsh <saturdaysyouth@gmail.com>
2022-07-11 16:21:56 +0300

    navbar update

--- src/components/static/Navbar.tsx
+++ src/components/static/Navbar.tsx
@@ -56,11 +56,10 @@ export function Navbar({
           <Link passHref href="/">
             <a>
               <Image
-                className={styles.color}
-                src={"/ampersand_white.svg"}
+                src={"/ampersand.svg"}
                 layout="fixed"
-                width="23"
-                height="23"
+                width="50"
+                height="50"
                 alt="logo-home"
                 priority
                 onClick={() => setIsOpen(false)}
@@ -79,7 +78,7 @@ export function Navbar({
         <div
           onKeyDown={({ code }) => (code === "Enter" ? setIsOpen(!isOpen) : "")}
           tabIndex={0}
-          className={`${styles.hamburger_lines} ${styles.color}`}
+          className={styles.hamburger_lines}
           aria-label={!isOpen ? "Open Navbar" : "Close Navbar"}
         >
           <span className={`${styles.line} ${styles.line1}`} />
@@ -101,24 +100,23 @@ export function Navbar({
                 >
                   <a>
                     <div
-                      style={{ margin: "0 5px", fontSize: "17px" }}
+                      style={{ margin: "0 5px" }}
                       aria-label={loc === "de" ? "Deutsch" : "English"}
-                      className={loc === locale ? styles.underlined : ""}
+                      className={loc === locale ? styles.selected : ""}
                     >
-                      {loc.toUpperCase()}
+                      {loc}
                     </div>
                   </a>
                 </Link>
               ))
               .reverse()
-              .reduce((prev, curr) => [prev, "|", curr])}
+              .reduce((prev, curr) => [prev, "/", curr])}
           </div>
         </div>
         <div className={styles.menu_items}>
           <div id="search" className="control has-icons-right">
             <input
-              style={{ height: "50px", borderWidth: "2px" }}
-              className="input is-rounded is-dark"
+              className="input is-rounded"
               placeholder="search"
               value={searchInput}
               onChange={(event) => setSearchInput(event.target.value)}
@@ -130,50 +128,48 @@ export function Navbar({
               }}
             />
             <span
-              style={{ height: "50px", width: "50px" }}
-              className="icon is-right is-clickable"
               onClick={() => {
                 search();
                 setIsOpen(false);
               }}
+              className="icon is-right"
             >
-              <FontAwesomeIcon color="#363636" icon={faSearch} />
+              <i className="fas fa-envelope">
+                <FontAwesomeIcon icon={faSearch} />
+              </i>
             </span>
           </div>
           <h3
             className="is-hidden-tablet"
             onClick={() => handleOnClick("/")}
-            style={{ marginTop: "85px", marginBottom: "30px" }}
+            style={{ marginTop: "85px" }}
           >
             Home
           </h3>
           <div className="is-hidden-tablet">
             <h2>{content.find_support}</h2>
-            <p onClick={() => handleOnClick("/result_2")}>
+            <h3 onClick={() => handleOnClick("/result_2")}>
               {content.for_domestic}
-            </p>
-            <p
+            </h3>
+            <h3
               onClick={() => handleOnClick("/result_1")}
               style={{ marginBottom: "30px" }}
             >
               {content.for_sexual}
-            </p>
+            </h3>
 
             <h2>{content.good_to_know}</h2>
-            <p onClick={() => handleOnClick("/domestic_general")}>
+            <h3 onClick={() => handleOnClick("/domestic_general")}>
               {content.about_domestic}
-            </p>
-            <p
+            </h3>
+            <h3
               onClick={() => handleOnClick("/sexual_general")}
               style={{ marginBottom: "30px" }}
             >
               {" "}
               {content.about_sexual}
-            </p>
-            <h3
-              style={{ marginBottom: "30px" }}
-              onClick={() => handleOnClick("/safety_tips")}
-            >
+            </h3>
+            <h3 onClick={() => handleOnClick("/safety_tips")}>
               {content.safety_tips}
             </h3>
             <h3
@@ -183,20 +179,20 @@ export function Navbar({
               {content.settings}
             </h3>
             <div className={styles.wrapper}>
-              <h3>{content.about_louandyou}</h3>
+              <h2>{content.about_louandyou}</h2>
               <Dropdown
                 content={<Text blok={content} attribute={"navbar_dropdown1"} />}
               />
             </div>
             <div className={styles.wrapper}>
-              <h3>{content.contact}</h3>
+              <h2>{content.contact}</h2>
               <Dropdown
                 content={
                   <>
                     <Text blok={content} attribute={"navbar_dropdown2"} />
-                    <p onClick={() => setIsFeedbackOpen(!isFeedbackOpen)}>
+                    <h3 onClick={() => setIsFeedbackOpen(!isFeedbackOpen)}>
                       Feedback
-                    </p>
+                    </h3>
                   </>
                 }
               />
@@ -204,17 +200,17 @@ export function Navbar({
           </div>
 
           <div
-            style={{ marginTop: "135px" }}
+            style={{ marginTop: "90px" }}
             className="is-hidden-mobile columns"
           >
             <div className="column">
               <h2>{content.find_support}</h2>
-              <p onClick={() => handleOnClick("/result_2")}>
+              <h3 onClick={() => handleOnClick("/result_2")}>
                 {content.for_domestic}
-              </p>
-              <p onClick={() => handleOnClick("/result_1")}>
+              </h3>
+              <h3 onClick={() => handleOnClick("/result_1")}>
                 {content.for_sexual}
-              </p>
+              </h3>
 
               <h2 style={{ marginTop: "90px" }}>{content.about_louandyou}</h2>
               <Text blok={content} attribute={"navbar_dropdown1"} />
@@ -222,23 +218,22 @@ export function Navbar({
 
             <div className="column">
               <h2>{content.good_to_know}</h2>
-              <p onClick={() => handleOnClick("/domestic_general")}>
+              <h3 onClick={() => handleOnClick("/domestic_general")}>
                 {content.about_domestic}
-              </p>
-              <p onClick={() => handleOnClick("/sexual_general")}>
+              </h3>
+              <h3 onClick={() => handleOnClick("/sexual_general")}>
                 {" "}
                 {content.about_sexual}
-              </p>
+              </h3>
 
               <h2 style={{ marginTop: "90px" }}>{content.contact}</h2>
               <Text blok={content} attribute={"navbar_dropdown2"} />
-              <p onClick={() => setIsFeedbackOpen(!isFeedbackOpen)}>Feedback</p>
+              <h3 onClick={() => setIsFeedbackOpen(!isFeedbackOpen)}>
+                Feedback
+              </h3>
             </div>
-            <div className="column">
-              <h3
-                style={{ marginBottom: "25px" }}
-                onClick={() => handleOnClick("/safety_tips")}
-              >
+            <div style={{ marginTop: "33px" }} className="column">
+              <h3 onClick={() => handleOnClick("/safety_tips")}>
                 {content.safety_tips}
               </h3>
               <h3 onClick={() => handleOnClick("/settings")}>
