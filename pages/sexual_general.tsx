1de1b15fdf4bc989e00bc198d8dc1e4b1c40ca12
Nadav Kirsh <saturdaysyouth@gmail.com>
2022-07-12 16:17:57 +0300

    general pages update

--- pages/sexual_general.tsx
+++ pages/sexual_general.tsx
@@ -1,16 +1,30 @@
 import styles from "./result.module.scss";
 import { pageGetStaticProps } from "../src/lib/pageGetStaticProps";
 import { PageContent, Text } from "../src/components/dynamic";
+import Link from "next/link";
 
 export default function Page({ story }) {
   const { content } = story;
   return (
     <>
-      <PageContent blok={content} name={"section_1"} />
+      <section className={styles.landing_page}>
+        <h1>{content.headline}</h1>
+        <Text blok={content} attribute={"subline"} />
+      </section>
       <section className={`${styles.white_page_full} is-hidden-tablet`}>
         <Text blok={content} attribute={"section_2"} />
       </section>
       <PageContent blok={content} name={"body"} />
+      <section className={styles.warning}>
+        <Text blok={content} attribute={"last"} />
+        <div className="is-flex-tablet is-justify-content-center">
+          <Link passHref href={"./result_2"}>
+            <button className={`${styles.button} ${styles.white}`}>
+              {content.last_btn}
+            </button>
+          </Link>
+        </div>
+      </section>
     </>
   );
 }
