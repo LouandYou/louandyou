aefa621c1691c52f3141c66f95d8a12f32219ee1
Nadav Kirsh <saturdaysyouth@gmail.com>
2022-07-20 13:37:17 +0300

    cleanup

--- src/components/static/Navbar.tsx
+++ src/components/static/Navbar.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import React, { ReactElement, useState } from "react";
-import Image from "next/image";
 
 import useScrollingBackgroundColor from "../../utils/useScrollingBackgroundColor";
 import styles from "./Navbar.module.scss";
