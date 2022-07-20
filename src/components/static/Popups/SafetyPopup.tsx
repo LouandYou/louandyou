9420992325521183d956d22dcfd78154f6d7e94c
Nadav Kirsh <saturdaysyouth@gmail.com>
2022-07-20 12:07:55 +0300

    style update

--- src/components/static/Popups/SafetyPopup.tsx
+++ src/components/static/Popups/SafetyPopup.tsx
@@ -26,12 +26,12 @@ export const SafetyPopup = ({
           <>
             <div className={`${styles.container} is-hidden-tablet`}>
               <Link href="tel:112" passHref>
-                <button className={`mb-3 ${styles.button} ${styles.orange}`}>
+                <button className={`mb-3 ${styles.button} ${styles.purple}`}>
                   {content.call_ambulance}
                 </button>
               </Link>
               <Link href="tel:110" passHref>
-                <button className={`mb-3 ${styles.button} ${styles.orange}`}>
+                <button className={`mb-3 ${styles.button} ${styles.purple}`}>
                   {content.call_police}
                 </button>
               </Link>
@@ -39,10 +39,10 @@ export const SafetyPopup = ({
 
             <div className="is-hidden-mobile">
               <p className="is-flex">
-                {content.ambulance}:<p className={styles.orange_text}>112</p>
+                {content.ambulance}:<p className={styles.purple_text}>112</p>
               </p>
               <p className="is-flex py-1">
-                {content.police}: <p className={styles.orange_text}>110</p>
+                {content.police}: <p className={styles.purple_text}>110</p>
               </p>
             </div>
           </>
@@ -51,7 +51,7 @@ export const SafetyPopup = ({
           <>
             <div className={`${styles.container} is-hidden-tablet`}>
               <Link href="tel:08000116116" passHref>
-                <button className={` ${styles.button} ${styles.orange_border}`}>
+                <button className={` ${styles.button} ${styles.purple_border}`}>
                   {content.hotline}
                 </button>
               </Link>
@@ -59,12 +59,9 @@ export const SafetyPopup = ({
             <div className="is-hidden-mobile">
               <p className="is-flex">
                 {content.Hilfetelefon}:
-                <p className={styles.orange_text}> 08000 116116</p>
+                <p className={styles.purple_text}> 08000 116116</p>
               </p>
             </div>
-            {/* <p style={{ color: "#979797", marginBottom: "30px" }}>
-              {content.safety_p1}
-            </p> */}
 
             <p style={{ marginTop: "32px" }} className="mb-2">
               {content.safety_p3}
