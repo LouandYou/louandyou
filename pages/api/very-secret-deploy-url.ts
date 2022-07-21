1b671a1c5987090c620c2bb3296300708151a330
Christoph Pröschel <chris@airy.co>
2022-07-21 16:52:49 +0200

    fix deploy url

--- pages/api/very-secret-deploy-url.ts
+++ pages/api/very-secret-deploy-url.ts
@@ -3,8 +3,8 @@ import axios from "axios";
 export default async function search(req, res) {
   try {
     await axios.post('https://api.netlify.com/build_hooks/62612c00702fc44159e2aead')
-    return res.status(200).text("Successfully triggered a new deploy");
+    return res.status(200).send("Successfully triggered a new deploy");
   } catch (e) {
-    return res.status(500).text("Unable to trigger a new deploy. Error: " + e);
+    return res.status(200).send("Unable to trigger a new deploy. Error: " + e);
   }
 }
